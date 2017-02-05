dc843362f0d005284e5b3a1d24008578d25cb9da
Jeffrey Trevino <jeffrey.trevino@gmail.com>
2017-02-04 16:14:10 -0800

    fixed test suite call

--- build_test.sh
+++ build_test.sh
@@ -3,9 +3,7 @@
 # Copyright 2016 Christopher Antila
 
 echo "Running all the test suites"
-cd julius
-npm test
-cd ..
+cd julius && npm test && cd ..
 py.test fujian
 py.test lychee
 py.test mercurial-hug
