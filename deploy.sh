fd20a97bb67f6b5019bae6b9155da70fb144cfe7
Jeffrey Trevino <jeffrey.trevino@gmail.com>
2017-05-07 09:27:14 -0700

    git rm-ed several deleted files

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-GH_REPO="@github.com/nCoda/macOS.git"
-
-FULL_REPO="https://$DEPLOY_TOKEN$GH_REPO"
-
-git add nCoda.dmg
-git commit -m "deployed to github pages"
-git push $FULL_REPO main
