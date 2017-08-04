647f4fc2fe09ab9b760b69e900a052a228f9c969
Jeffrey Trevino <jeffrey.trevino@gmail.com>
2017-08-04 15:27:17 -0700

    revised dirs in test script

--- build_test.sh
+++ build_test.sh
@@ -3,7 +3,10 @@
 # Copyright 2016 Christopher Antila
 
 echo "Running all the test suites"
+# wd: macOS/
+cd build/programs
 cd julius && npm test && cd ..
 cd fujian && py.test && cd ..
 # cd lychee && py.test && cd ..
 cd mercurial-hug && py.test && cd ..
+cd ../..
