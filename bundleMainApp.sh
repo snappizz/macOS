17d9a0cfb0dd44b6888939a54995a693479b37a1
Jeffrey Trevino <jeffrey.trevino@gmail.com>
2017-08-04 15:07:10 -0700

    changed dmg directory to macOS/dist

--- bundleMainApp.sh
+++ bundleMainApp.sh
@@ -13,18 +13,18 @@ then
     cp electron_main_production.js build/
     cp production_requirements.txt build/
     cp package.json build/
-    cd build
+    cd build # -> macOS/build/
     virtualenv backend_venv
     source backend_venv/bin/activate
     pip install -r production_requirements.txt
     mkdir programs
-    cd programs
+    cd programs # -> macOS/build/programs/
     git clone https://github.com/nCoda/ShelfExtender.git ShelfExtender
     git clone https://github.com/nCoda/hgdemo_config.git hgdemo_config
     cd ShelfExtender && pip install . && cd ..
     cd hgdemo_config && python -m shelfex && cd ..
     cp -r hgdemo_config/repo hgdemo
-    cd ..
+    cd .. # -> macOS/build/
 
 else
     cp nCoda.icns build/
@@ -36,7 +36,7 @@ else
     rm -rf build/build
     rm -rf build/dist
     rm -rf build/nCoda.dmg
-    cd build
+    cd build # -> macOS/build/
     source backend_venv/bin/activate
 fi
 
@@ -49,7 +49,7 @@ fi
 if ! [ -d julius ]
 then
     git clone --recursive https://github.com/nCoda/julius.git julius
-    cd julius && npm install && cd ..
+    cd julius && npm install && cd .. # -> macOS/build/
 fi
 # build Electron app
 
@@ -61,6 +61,9 @@ mv electron_main_production.js julius/js/electron_main.js
 rm -rf julius/nCoda-darwin-x64
 cd julius && electron-packager . --icon=../nCoda.icns && cd ..
 cp -r dist/nCoda.app julius/nCoda-darwin-x64/nCoda.app/Contents/Resources/
-hdiutil create -srcfolder julius/nCoda-darwin-x64/nCoda.app dist/nCoda.dmg
-cd .. 
+cd ..
+# wd: macOS/
+mkdir dist && cd dist # -> macOS/dist
+hdiutil create -srcfolder ../build/julius/nCoda-darwin-x64/nCoda.app nCoda.dmg
 deactivate
+cd .. # -> macOS/
